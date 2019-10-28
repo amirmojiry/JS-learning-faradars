d6e9ea88a31fa53f537d56c8f86fe2805bd9d9a7
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2019-10-28 15:29:18 +0330

    lesson 3

---
+++ functions.js
@@ -0,0 +1,20 @@
+function changeText ()
+{
+    document.getElementById("p1").innerHTML = "Bye";
+}
+
+var names = new Array();
+var student = {
+    firstName : "ali",
+    age: 22
+};
+var x = prompt ("Enter a number:");
+
+switch (x) {
+    case "a" : 
+        alert ("great");
+        break;
+    default: 
+        alert ("bad");
+        break;
+}
