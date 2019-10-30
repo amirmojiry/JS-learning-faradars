07771a179ca3877dbae7df23d3fbe606938b6340
Amirhossein Mojiri Foroushani <amojiry@gmail.com>
2019-10-30 11:33:16 +0330

    write a simple calculator function

--- functions.js
+++ functions.js
@@ -1,20 +1,25 @@
-function changeText ()
-{
-    document.getElementById("p1").innerHTML = "Bye";
+function getNumbers () {
+    var n1 = parseInt (prompt ("Enter first number: "));
+    var n2 = parseInt (prompt ("Enter second number: "));
+    var op = prompt ("Enter operator: ");
+    var result  = calc (n1, n2, op);
+    alert (result);
 }
 
-var names = new Array();
-var student = {
-    firstName : "ali",
-    age: 22
-};
-var x = prompt ("Enter a number:");
-
-switch (x) {
-    case "a" : 
-        alert ("great");
-        break;
-    default: 
-        alert ("bad");
-        break;
+function calc (num1, num2, operator) {
+    if (num2 == 0 && operator == "/") {
+        return "Divide zero error";
+    }
+    switch (operator) {
+        case "+":
+            return (num1+num2);
+        case "-":
+            return (num1-num2);
+        case "*":
+            return (num1*num2);
+        case "/":
+            return (num1/num2);
+        default:
+            return "Please enter a valid operator (+, -, *, /)";
+    }
 }
